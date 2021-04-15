f3e2fc85e89cdac6221668bb425f580763a0ad9e
Matthew Heuer <82069045+MattHeuer@users.noreply.github.com>
2021-04-15 13:46:44 +0800

    Update BulkPasswordReset.ps1

--- BulkPasswordReset.ps1
+++ BulkPasswordReset.ps1
@@ -1,5 +1,5 @@
 $mycreds = Get-Credential
-$accounts = "UONET1","UTWOT1","UTHRET1","UFOURT1","UFIVET1","USIXT1","USEVET1","UEIGHT1","UNINET1","UTENT1","UELEVT1","UTWELT1","UTHIRT1","UFOURT2","UFIFTT1","USIXTT1","USEVET2","UEIGHT2","UNINET2","UTWENT0","UTWENT1","UTWENT2","UTWENT3","UTWENT4"
+$accounts = "#SamAccountName#","#SamAccountName#","#SamAccountName#"
 foreach ($SAM in $accounts)
 {
     Set-ADAccountPassword $SAM -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "PASSWORD" -Force) -Credential $mycreds
